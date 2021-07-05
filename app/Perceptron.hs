992f51c2cbf6f5389c80aa767995f8de66a40ff3
Edward <edward@correctsoftware.co>
2021-07-05 08:51:14 +0100

    More work on perceptron

--- app/Perceptron.hs
+++ app/Perceptron.hs
@@ -1,14 +1,25 @@
-module Perceptron where
-
 {-# LANGUAGE TemplateHaskell #-}
+
+module Perceptron where
 
 import Development.Placeholders
 
-data InputSignals = InputSignals [Float] deriving (Show)
-data Weights = Weights [Float] deriving (Show)
+type Signal = Float
+type Weight = Float
+type WeightedSignal = Float
 
-weightedSum = InputSignals -> Weights -> Int
-weightedSum inputSignals weights = $notImplemented
+data InputSignals = InputSignals [Signal] deriving (Show)
+data Weights = Weights [Weight] deriving (Show)
+data WeightedInputSignals =  WeightedInputSignals [WeightedSignal] deriving (Show)
+
+initialiseWeights is = InputSignals -> Weights
+initialiseWeights is = Weights . replicate (length is) 0
+
+weightedInputSignals = InputSignals -> Weights -> WeightedInputSignals
+weightedInputSignals is w = zipWith (*) is w
+
+weightedSum = WeightedInputSignals -> Int
+weightedSum wis = foldl' (+) 0 wis
 
 activationFunction x = Int -> Int
 activationFunction x
